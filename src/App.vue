c37e24751ab81b439f012d982bce0af608c0c18f
Ankhyang <ankh3690@163.com>
2019-12-29 17:46:32 +0800

    静态界面功能开发

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,24 @@
 <template>
-  <div>
-    app
+  <div id="app">
+    <router-view/>
+    <FootGuide/>
   </div>
 </template>
 
 <script type="text/javascript">
+import FootGuide from './components/FooterGuide/FooterGuide'
 export default {
+  components: {FootGuide},
   data () {
     return {}
   }
 }
 </script>
 
-<style lang="stylus" scoped></style>
+<style lang="stylus" scoped>
+  #app
+    width 100%
+    height 100%
+    background #f5f5f5
+    position relative
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App'
+import router from './router'
+import 'swiper/dist/css/swiper.css'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+
+Vue.use(VueAwesomeSwiper)
 
 // eslint-disable-next-line no-new
 new Vue({
   el: '#app',
-  render: h => h(App)
+  render: h => h(App),
+  router
 })
